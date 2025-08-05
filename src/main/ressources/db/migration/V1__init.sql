35aa4c279cc55f26709acfea833c89ed137f0aa7
Jikel <hei.jikel.2@gmail.com>
2025-08-05 14:26:43 +0300

    feat: create sql script for database migration

---
+++ src/main/ressources/db/migration/V1__init.sql
@@ -0,0 +1,47 @@
+-- Création de la table donor
+CREATE TABLE donor (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       full_name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Création de la table payment
+CREATE TABLE payment (
+                         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                         payment_id VARCHAR(255) NOT NULL UNIQUE,
+                         date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                         status VARCHAR(50) NOT NULL,
+                         amount DOUBLE PRECISION NOT NULL,
+                         method VARCHAR(100) NOT NULL
+);
+
+-- Création de la table donation
+CREATE TABLE donation (
+                          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                          donor_id UUID NOT NULL,
+                          date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                          amount DOUBLE PRECISION NOT NULL,
+                          payment_method VARCHAR(100) NOT NULL,
+                          payment_id UUID NOT NULL,
+                          CONSTRAINT fk_donor FOREIGN KEY(donor_id) REFERENCES donor(id),
+                          CONSTRAINT fk_payment_donation FOREIGN KEY(payment_id) REFERENCES payment(id)
+);
+
+-- Création de la table beneficiary
+CREATE TABLE beneficiary (
+                             id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                             full_name VARCHAR(255) NOT NULL,
+                             email VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Création de la table help
+CREATE TABLE help (
+                      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                      beneficiary_id UUID NOT NULL,
+                      date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                      amount DOUBLE PRECISION NOT NULL,
+                      accident_description TEXT NOT NULL,
+                      payment_id UUID NOT NULL,
+                      CONSTRAINT fk_beneficiary FOREIGN KEY(beneficiary_id) REFERENCES beneficiary(id),
+                      CONSTRAINT fk_payment_help FOREIGN KEY(payment_id) REFERENCES payment(id)
+);
